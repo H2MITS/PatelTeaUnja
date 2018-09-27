26a233a0abf1dafb39b37960a787b69020514579
Harsh Soni <Soni.harsh38@live.com>
2018-09-27 19:47:10 +0530

    Added Coming Soon Page and made changes for url extesnsion. added nudget package

--- PatelTeaSource/PatelTeaSource/Site.Mobile.Master.cs
+++ PatelTeaSource/PatelTeaSource/Site.Mobile.Master.cs
@@ -1,13 +1,13 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace PatelTeaSource.ClientSide
+namespace PatelTeaSource
 {
-    public partial class Contactus : System.Web.UI.Page
+    public partial class Site_Mobile : System.Web.UI.MasterPage
     {
         protected void Page_Load(object sender, EventArgs e)
         {
